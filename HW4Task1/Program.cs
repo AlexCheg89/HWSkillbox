ae1247c77ceb4f3b713439ce4c2457db6478336e
AlexCheg89 <aleksejchegodaev@yandex.ru>
2023-10-14 15:33:50 +0500

    update HW4Task1

--- HW4Task1/Program.cs
+++ HW4Task1/Program.cs
@@ -10,6 +10,43 @@ namespace HW4Task1
     {
         static void Main(string[] args)
         {
+            // Ввод количества строк будущей матрицы
+
+            Console.Write("Введите количество столбцов: ");
+            int row = int.Parse(Console.ReadLine());
+
+            // Ввод количетва столбцов в будущей матрицы
+
+            Console.Write("Введите количество столбцов: ");
+            int col = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int[,] matrix = new int[row, col];
+
+            Random r = new Random();
+
+            int sum = 0;
+
+            for (int i = 0; i < row; i++)
+            {
+                int rowSum = 0;
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = r.Next(10);          // Заполнение матрицы случайными числами
+                    Console.Write($"{matrix[i,j]} ");   // Вывод полученной матрицы на экран
+                    rowSum += matrix[i, j];
+                }
+
+                Console.WriteLine(); // Вывод полученной матрицы на экран
+
+                sum += rowSum;
+            }
+            Console.WriteLine();
+
+            // Вывод суммы всех элементов этой матрицы она экран
+            Console.WriteLine($"Сумма всех элементов матрицы: {sum}");
+
+            Console.ReadKey();
         }
     }
 }
