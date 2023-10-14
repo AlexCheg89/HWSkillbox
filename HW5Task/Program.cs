4c2814633d73c13c04db6ad842aa7d46b795eeaf
AlexCheg89 <aleksejchegodaev@yandex.ru>
2023-10-14 17:39:01 +0500

    update HW5Task

--- HW5Task/Program.cs
+++ HW5Task/Program.cs
@@ -8,8 +8,55 @@ namespace HW5Task
 {
     internal class Program
     {
+        /// <summary>
+        /// Разделение по словам
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string[] StringToWord(string text)
+        {
+            string[] words = text.Split(' ');
+
+            return words;
+        }
+
+        /// <summary>
+        /// Вывод слов в консоль
+        /// </summary>
+        /// <param name="text"></param>
+        static void OutWordToConsole(string[] text)
+        {
+            foreach (var word in text)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        /// <summary>
+        /// Изменение порядка слов
+        /// </summary>
+        /// <param name="text"></param>
+        static void RewersWords (string text) 
+        {
+            string[] new_text = StringToWord(text);
+
+            for (int i = new_text.Length -1;  i >= 0; i--)
+            {
+                Console.Write($"{new_text[i]} ");
+            }
+        }        
+
         static void Main(string[] args)
         {
+            Console.Write("Введите предложение: ");
+            string textofconsol = Console.ReadLine();
+
+            OutWordToConsole(StringToWord(textofconsol));
+            Console.WriteLine();
+
+            RewersWords(textofconsol);
+
+            Console.ReadKey();
         }
     }
 }
