65a613713e6428b2f870159dc9eecc2f0c98009f
AlexCheg89 <aleksejchegodaev@yandex.ru>
2023-10-14 11:26:37 +0500

    update HWTask2

--- HWTask2/Program.cs
+++ HWTask2/Program.cs
@@ -10,6 +10,16 @@ namespace HWTask2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("// Вывод с использованием метода WriteLine");
+            Console.WriteLine("Hello World!!!");
+            Console.WriteLine(); // Пустая строка разделитель
+
+            Console.WriteLine("// Вывод с использованием метода Write");
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+
+            Console.ReadKey();
         }
     }
 }
