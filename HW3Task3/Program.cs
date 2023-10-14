ef2861fdea96cb9b0a857da3c5f24202f49947e5
AlexCheg89 <aleksejchegodaev@yandex.ru>
2023-10-14 13:48:53 +0500

    Update HW3Task3

--- HW3Task3/Program.cs
+++ HW3Task3/Program.cs
@@ -10,6 +10,24 @@ namespace HW3Task3
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите целое число ");
+            int n = int.Parse(Console.ReadLine());
+            int i = 2;
+
+            bool result = false;
+
+            while (i <= n-1) 
+            { 
+                if (n%i==0) 
+                    result = true;
+                
+                i++;
+            }
+
+            if (result == true) Console.WriteLine($"{n} - Не простое число!");
+            else Console.WriteLine($"{n} - Просто число!");
+
+            Console.ReadKey();           
         }
     }
 }
