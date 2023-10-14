4dd5f3fe20b3083638c53e5a252b88a4d71f98b0
AlexCheg89 <aleksejchegodaev@yandex.ru>
2023-10-14 16:13:38 +0500

    update HW4Task3

--- HW4Task3/Program.cs
+++ HW4Task3/Program.cs
@@ -10,6 +10,45 @@ namespace HW4Task3
     {
         static void Main(string[] args)
         {
+            // Ввод максимального числа диапазона (целое число)
+            Console.Write("Введите максимальное число диапазона: ");
+            int max_range_numb = int.Parse(Console.ReadLine());
+                
+            // Генерация случайных чисел (целое число) от 0 до введенного числа
+            Random numb = new Random();
+            var cpuNumb = numb.Next(0, max_range_numb);
+
+            // Ввод пользователем числа
+            while (true) 
+            {
+                ConsoleKeyInfo userNumb = Console.ReadKey();
+
+                if (userNumb.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine($"\nПопробуйте в другой раз. Загаданное число {cpuNumb}");
+                    break;
+                }
+
+                if (int.Parse(userNumb.KeyChar.ToString()) < cpuNumb)
+                {
+                    Console.WriteLine("\nВведенное число меньше загаданного");
+                    continue;
+                }
+
+                else if (int.Parse(userNumb.KeyChar.ToString()) > cpuNumb)
+                {
+                    Console.WriteLine("\nВведенное число больше загаданного");
+                    continue;
+                }
+
+                else if (int.Parse(userNumb.KeyChar.ToString()) == cpuNumb)
+                    Console.WriteLine($"\nВы угадали число {cpuNumb}");
+                break;
+
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
