1604d6a622aa4bb66fce334919ab5b22b130d659
AlexCheg89 <aleksejchegodaev@yandex.ru>
2023-10-14 12:50:24 +0500

    upadete HW3Task1

--- HW3Task3/Program.cs
+++ HW3Task3/Program.cs
@@ -10,6 +10,34 @@ namespace HW3Task3
     {
         static void Main(string[] args)
         {
+            int en_new_number;
+
+            do
+            {
+                Console.Write("Введите целое число ");
+
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0)
+
+                    Console.WriteLine($"Введеное число {number} чётное");
+
+                else
+                    Console.WriteLine($"ВВеденое число {number} нечётное");
+
+                Console.WriteLine();
+                Console.Write("Чтобы повторить ввод чисел нажмите 1 ");
+                en_new_number = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+            }
+            while (en_new_number == 1);
+
+            Console.WriteLine("Досвидание!!!");
+            
+            
+            Console.ReadKey();
+
         }
     }
 }
