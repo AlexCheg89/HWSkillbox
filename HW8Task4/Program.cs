522fd00832f2d3eee95edef9045ba4475f3b4262
AlexCheg89 <aleksejchegodaev@yandex.ru>
2023-10-15 12:36:25 +0500

    update HW8Task4

--- HW8Task4/Program.cs
+++ HW8Task4/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+
 
 namespace HW8Task4
 {
@@ -10,6 +11,43 @@ namespace HW8Task4
     {
         static void Main(string[] args)
         {
+            string path = "PhoneBook.xml";
+
+            Console.Write("Введите ФИО: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите улицу: ");
+            string street = Console.ReadLine();
+
+            Console.Write("Введите номер дома: ");
+            uint houseNumber = Convert.ToUInt32(Console.ReadLine());
+
+            Console.Write("Введите номер квартиры: ");
+            uint flatNumber = Convert.ToUInt32(Console.ReadLine());
+
+            Console.Write("Введите номер мобильного телефона: ");
+            string mobilePhone = Console.ReadLine();
+
+            Console.Write("Введите номер домашнего телефона: ");
+            string flatPhone = Console.ReadLine();
+
+            XDocument xdoc = new XDocument(new XElement("PhoneBook",
+                new XElement("Person",
+                    new XAttribute("Name", name),
+
+                new XElement("Address",
+                    new XElement("Street", street),
+                    new XElement("HouseNumber", houseNumber),
+                    new XElement("FlatNumber", flatNumber),
+                new XElement("Phones",
+                    new XElement("MobilePhone", mobilePhone),
+                    new XElement("FlatPhone", flatPhone))))));
+            
+            xdoc.Save(path);
+
+            Console.WriteLine("Файл создан");
+
+            Console.ReadKey();
         }
     }
 }
