78f8adbc856b222ce7f220a158032b60508e688f
AlexCheg89 <aleksejchegodaev@yandex.ru>
2023-10-15 12:04:42 +0500

    update HW8Task3

---
+++ HW8Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW8Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>(new int[] { 10 });
+
+            int numberOfConsole;
+
+            Console.Write($"Введите число: ");
+            numberOfConsole = Convert.ToInt32(Console.ReadLine());
+
+            if (set.Contains(numberOfConsole))
+            {
+                Console.WriteLine($"Число {numberOfConsole} присутствует в коллекции");
+            }
+            else
+            {
+                Console.WriteLine($"Число {numberOfConsole} добавлено в коллекцию");
+                set.Add(numberOfConsole);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
