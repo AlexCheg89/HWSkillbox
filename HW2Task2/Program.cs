7a997c2b0bc52ddfae7d3f730aa1f2e7d7db8daa
AlexCheg89 <aleksejchegodaev@yandex.ru>
2023-10-15 11:49:39 +0500

    update HW8Task2

--- HW2Task2/Program.cs
+++ HW2Task2/Program.cs
@@ -8,8 +8,68 @@ namespace HW2Task2
 {
     internal class Program
     {
+        /// <summary>
+        /// Ввод записи номер и ФИО
+        /// </summary>
+        /// <returns></returns>
+        static (string, string) EnterPhoneBookRecord()
+        {
+            Console.Write("Введите номер телефона (9XXXXXXXXX): ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Введите ФИО Владельца: ");
+            string fio = Console.ReadLine();
+            var result = (phoneNumber, fio);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Поиск владельца по номеру
+        /// </summary>
+        /// <param name="phoneBook"></param>
+        static void NumberOwnerSearching(Dictionary<string, string> phoneBook) 
+        {
+            Console.WriteLine($"Поиск по номеру телефона");
+            Console.Write($"Введите номер телефона (9XXXXXXXXX): ");
+            string phoneNumber = Console.ReadLine();
+
+            if (phoneBook.TryGetValue(phoneNumber, out string fio))
+            {
+                Console.WriteLine($"ФИО владельца: {phoneBook[phoneNumber]}");
+            }
+            else Console.WriteLine($"$Владелец номера {phoneNumber} не зарегистрирован");
+        }
+
+        static void OutputToConsole(Dictionary<string, string> phoneBook)
+        {
+            foreach (KeyValuePair<string, string> e in phoneBook) Console.WriteLine($"{e} );
+        }
+
         static void Main(string[] args)
         {
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+            char choice = 'н';
+
+            // Ввод записей и добавление в коллекцию Dictionary c добаэвлением новой записи
+
+            do
+            {
+                var tuple = EnterPhoneBookRecord();
+                phoneBook.Add(tuple.Item1, tuple.Item2);
+
+                Console.Write("Ввести новую запись? (д/н): ");
+                choice = Convert.ToChar(Console.ReadLine());
+
+            } while (choice == 'д');
+
+            // Вывод содердимого на экран
+            OutputToConsole(phoneBook);
+
+            // Поиск владельца по введенному номеру
+            NumberOwnerSearching(phoneBook);
+
+            Console.ReadKey();
         }
     }
 }
